92772526b085d6a27d1e95ff6cbc9182e0cddbe8
Jishnu M M <jishnumm4312@gmail.com>
2024-03-25 01:22:40 +0530

    added files

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -5,7 +5,7 @@ const AuthContext = React.createContext()
 
 //const serverUrl = 'http://localhost:5000/api'
 // const serverUrl = 'https://agroscape-server-967ee60b99c8.herokuapp.com/api'
-const serverUrl = 'https://medbot-backend.vercel.app/api/'
+const serverUrl = 'https://medbot-backend.vercel.app/api'
 
 export const AuthContextProvider = ({children}) => {
 
